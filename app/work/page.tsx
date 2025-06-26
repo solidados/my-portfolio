caf57501078fd5a82af1bd911f6ba2c98ffbde11
solidados <geekbrains.pk@internet.ru>
2025-06-26 17:32:35 +0400

    feat: add work page and style it

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,174 @@
-import { FC } from "react";
+"use client";
 
+import { FC, useState } from "react";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type { Swiper as SwiperType } from "swiper";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import "swiper/css";
+
+type TStack = {
+  name: string;
+};
+
+interface IProject {
+  num: string;
+  category: string;
+  title: string;
+  description: string;
+  stack: TStack[];
+  image: string;
+  live: string;
+  github: string;
+}
+
+const projects: IProject[] = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque error officia reiciendis.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "https://github.com/solidados",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    stack: [{ name: "React" }, { name: "Typescript" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "backend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, corporis explicabo harum nisi nostrum quaerat quasi reprehenderit sed vero voluptatem. Aliquam cupiditate est facere inventore.",
+    stack: [
+      { name: "Nest.js" },
+      { name: "Prisma" },
+      { name: "typeORM" },
+      { name: "Typescript" },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "https://github.com/solidados",
+  },
+];
+
+// https://youtu.be/dImgZ_AH7uA?si=5ukCbqF7jGRBUmt3&t=8289
 const Work: FC = () => {
-  return <div>Work</div>;
+  const [project, setProject] = useState<IProject>(projects[0]);
+
+  const handleSlideChange = (swiper: SwiperType) => {
+    // get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[2rem]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[2rem] h-[50%] select-none">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[2.625rem] leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => (
+                  <li key={index} className="text-xl text-accent">
+                    {item.name}
+                    {index !== project.stack.length - 1 && ","}
+                  </li>
+                ))}
+              </ul>
+              <div className="border border-white/20" />
+              <div className="flex items-center gap-4">
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[4.375rem] h-[4.375rem] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {project.github && (
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[4.375rem] h-[4.375rem] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>GitHub repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div></div>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={project.image}
+                        fill
+                        className="object-cover overflow-hidden"
+                        alt={project.title}
+                      />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Work;
