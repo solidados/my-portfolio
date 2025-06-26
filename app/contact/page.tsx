770c596ef6fb08223d00ce88c798fa70e642e0a1
solidados <geekbrains.pk@internet.ru>
2025-06-26 20:47:12 +0400

    feat: complete `Contact` page and styles

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,109 @@
+"use client";
+
 import { FC } from "react";
+import { motion } from "framer-motion";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+374) 55 43 47 41",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "pavel.konyakhin@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Location",
+    description: "Republic of Armenia",
+  },
+];
 
 const Contact: FC = () => {
-  return <div>Contact Page</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[2rem]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent select-none">
+                Let&apos;s work together
+              </h3>
+              <p className="text-white/60 select-none">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Cupiditate eum inventore natus nihil nostrum!
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First name" />
+                <Input type="lastname" placeholder="Last name" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select service..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Service select</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                    <SelectItem value="mst">Backend Development</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[200px] w-full"
+                placeholder="Your message here..."
+              />
+              <Button size="md" className="md:max-w-40">
+                Send message
+              </Button>
+            </form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div>{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <h3 className="text-white/60 select-none">{item.title}</h3>
+                    <p className="text-[1.1rem]">{item.description}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
@@ -9,14 +9,16 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
+        "flex min-h-[80px] w-full rounded-md border border-white/10 bg-primary p-4 text-base placeholder:text-white/60" +
+          " focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-accent focus-visible:ring-offset-0" +
+          " disabled:cursor-not-allowed disabled:opacity-50",
+        className,
       )}
       ref={ref}
       {...props}
     />
-  )
-})
-Textarea.displayName = "Textarea"
+  );
+});
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
